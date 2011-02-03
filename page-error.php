fe6017e6b2fe28265c02959a89e5fd5ca245a818
krer <e.kristoffer@gmail.com>
2011-02-03 20:50:37 +0100

    En sida

---
+++ page-error.php
@@ -0,0 +1,18 @@
+<section id="posts">
+		<article class="blogpost">
+		<?php if (is_search()): ?>
+				<h3>Search Results</h3>
+					<p>Your search - <strong><?PHP echo $_GET[s]; ?></strong> - did not match any documents. </p>
+					Suggestions:
+					<ul>
+						<li>Make sure all words are spelled correctly.</li>
+						<li>Try different keywords.</li>
+						<li>Try more general keywords.</li>
+						<li>Try fewer keywords.</li>
+					</ul>
+		<?php  else: ?>
+			<h3>We couldn't to find what your looking for</h3>
+			<p>The page you've requested can not be displayed. It appears you've missed your intended destination, either through a bad or outdated link, or a typo in the page you were hoping to reach.</p>
+		<?php endif; ?>
+		</article>
+</section>
