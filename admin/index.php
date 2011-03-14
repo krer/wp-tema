6ae180f4f6d5e6f262f81e8ff9de172f1454acdd
krer <e.kristoffer@gmail.com>
2011-03-14 18:56:37 +0100

    even more updates

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,9 @@
 <?php
 
-function tc_admin_head() { ?>
+function admin_head() { ?>
 		<style>
-		#tc-wrap { width: 740px; }
-		#pagetitle { font-style: italic; margin: 0;  color: #464646; font: italic 24px/35px Georgia,"Times New Roman","Bitstream Charter",Times,serif; padding-top: 14px; text-shadow: 0 1px 0 #fff; }
-		.admintitle { margin: 0px; padding: 10px; font-family: Georgia, serif; font-weight: normal; letter-spacing: 1px; font-size: 18px; border: 1px solid #e4e4e4; border-bottom: 0; background:#eee;  }
-		.maintable { background: #f9f9f9; margin-bottom: 20px; border: 1px solid #e4e4e4; }
-		.mainrow { padding: 10px; border-bottom: 1px solid #e4e4e4; float: left; }
-		.mainrow:last-child { border: 0; }
-		.titledesc { font-size: 12px; font-weight: 700; width: 180px; float: left;  }
-		.forminp { width: 740px; }
-		.forminp input, .forminp select, .forminp textarea { background: #fff; background: #f3f3f3; width: 280px; padding: 4px; font-size: 12px; float: left; }
-		.forminp span { font-size: 11px; float: right; width: 220px; }
-		.info { background: #FFFFCC; border: 1px dotted #D8D2A9; padding: 10px; color: #333; }
-		.forminp .checkbox { width: 20px }
-		.info a { color: #333; text-decoration: none; border-bottom: 1px dotted #333 }
-		.info a:hover { color: #666; border-bottom: 1px dotted #666; }
-		.warning { background: #FFEBE8; border: 1px dotted #CC0000; padding: 10px; color: #333; font-weight: bold; }
+			/* Include stylesheet */
+			<?php include(TEMPLATEPATH . "/admin/admin.css"); ?>
 		</style>
 
 	<?php 
@@ -25,13 +12,13 @@ function tc_admin_head() { ?>
 	$themename = "True colors";
 	$shortname = "tc";
 
-	function tc_generate_page($options){
+	function generate_page($options){
 		global $themename;
 		?>
 			<div id="tc-wrap">
     			<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
 						<h2 id="pagetitle"><?php echo $themename; ?> settings</h2>
-						<?php if ( $_REQUEST['saved'] ) { ?><div class="warning"><?php echo $themename; ?>'s options has been updated!</div><?php } ?>
+						<?php if ( $_REQUEST['saved'] ) { ?><div class="update"><?php echo $themename; ?>'s options has been updated!</div><?php } ?>
      						<table class="maintable">
 							<?php foreach ($options as $value) { ?>
 									<?php if ( $value['type'] <> "heading" ) { ?>
@@ -79,75 +66,75 @@ function tc_admin_head() { ?>
 								<input type="hidden" name="action" value="save">
 							</p>
             </form>
-</div><!--#tc_wrap-->
+</div><!--#tc-wrap-->
  <?php
 }
 	
-	$tcoptions = array();	
-	$tcoptions[] = array(	"name" => "General",
-							"type" => "heading");
-	$tcoptions[] = array(	"name" => "Custom logo",
-							"desc" => "Enter the link to your logo.",
-							"id" => $shortname."_logo",
-							"std" => "",
-							"type" => "text");
-	$tcoptions[] = array(	"name" => "Custom favicon",
-							"desc" => "Enter the link to your favicon.",
-							"id" => $shortname."_favicon",
-							"std" => "",
-							"type" => "text");
+	$options = array();	
+	$options[] = array(	"name" => "General",
+						"type" => "heading");
+	$options[] = array(	"name" => "Custom logo",
+						"desc" => "Enter the link to your logo.",
+						"id" => $shortname."_logo",
+						"std" => "",
+						"type" => "text");
+	$options[] = array(	"name" => "Custom favicon",
+						"desc" => "Enter the link to your favicon.",
+						"id" => $shortname."_favicon",
+						"std" => "",
+						"type" => "text");
 							
-	$tcoptions[] = array(	"name" => "Social networks",
-							"type" => "heading");
-	$tcoptions[] = array(	"name" => "Twitter username",
-							"desc" => "Enter your twitter username here.",
-							"id" => $shortname."_twitter",
-							"std" => "",
-							"type" => "text");
-	$tcoptions[] = array(	"name" => "Facebook username",
-							"desc" => "Enter your facebook username here.",
-							"id" => $shortname."_facebook",
-							"std" => "",	
-							"type" => "text");
-	$tcoptions[] = array(	"name" => "Email",
-							"desc" => "Enter your email here.",
-							"id" => $shortname."_email",
-							"std" => "",	
-							"type" => "text");		
+	$options[] = array(	"name" => "Social networks",
+						"type" => "heading");
+	$options[] = array(	"name" => "Twitter username",
+						"desc" => "Enter your twitter username here.",
+						"id" => $shortname."_twitter",
+						"std" => "",
+						"type" => "text");
+	$options[] = array(	"name" => "Facebook username",
+						"desc" => "Enter your facebook username here.",
+						"id" => $shortname."_facebook",
+						"std" => "",	
+						"type" => "text");
+	$options[] = array(	"name" => "Email",
+						"desc" => "Enter your email here.",
+						"id" => $shortname."_email",
+						"std" => "",	
+						"type" => "text");		
 							
-	$tcoptions[] = array(	"name" => "Copyright",
-							"type" => "heading");
-	$tcoptions[] = array(	"name" => "Footer copyright text",
-							"desc" => "Enter the text you wish to use in the footer.",
-							"id" => $shortname."_copytext",
-							"std" => "",
-							"type" => "textarea");		
+	$options[] = array(	"name" => "Copyright",
+						"type" => "heading");
+	$options[] = array(	"name" => "Footer copyright text",
+						"desc" => "Enter the text you wish to use in the footer.",
+						"id" => $shortname."_copytext",
+						"std" => "",
+						"type" => "textarea");		
 							
 							
-	function tc_index_options() {
-		global $tcoptions;
-		tc_generate_page($tcoptions);
+	function index_options() {
+		global $options;
+		generate_page($options);
 	}
 	
-	function tc_add_admin() {
-		global $themename,$tcoptions;
-		if ( $_GET['page'] == "tc-options") {
+	function add_admin() {
+		global $themename, $options;
+		if ( $_GET['page'] == "options") {
 			if ( 'save' == $_REQUEST['action'] ) {
-					foreach ($tcoptions as $value) {
+					foreach ($options as $value) {
 							update_option( $value['id'], $_REQUEST[ $value['id'] ] ); 
 					}
 					
-					foreach ($tcoptions as $value) {
+					foreach ($options as $value) {
 						if( isset( $_REQUEST[ $value['id'] ] ) ) { update_option( $value['id'], $_REQUEST[ $value['id'] ]  ); } else { delete_option( $value['id'] ); }
 					}
-					header("Location: admin.php?page=tc-options&saved=true");								
+					header("Location: admin.php?page=options&saved=true");								
 				die;
 			}
 		}
-		add_menu_page("Main Options", "Theme Options", 'edit_themes', 'tc-options', 'tc_index_options');
+		add_menu_page("Main Options", "Theme Options", 'edit_themes', 'options', 'index_options');
 	}
 	
-add_action('admin_menu', 'tc_add_admin');
-add_action('admin_head', 'tc_admin_head');	
+add_action('admin_menu', 'add_admin');
+add_action('admin_head', 'admin_head');	
 
 ?>
