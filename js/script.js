eb1e4cc6e92a6e110e932dab4a6c93c906b2f3f2
krer <e.kristoffer@gmail.com>
2011-03-18 14:03:47 +0100

    ie 8 fixes

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ jQuery.noConflict();
 jQuery(function(){
 
 	// Fix for ie8
+	jQuery("aside ul:first:child li").css("border-bottom","1px solid #e4e4e4");
    jQuery("aside li:last-child").css("border-bottom","none", "margin-bottom", 0, "padding-bottom", 0);
-
+   
 	// MouseOver Events
 	jQuery('.thumb').hover(function(){
 		jQuery('img', this).fadeTo("fast", 0.35).addClass('box-hover');},
