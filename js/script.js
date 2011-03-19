1502436db737e56ac5bfde88f2915cec3c04772c
krer <e.kristoffer@gmail.com>
2011-03-19 14:15:57 +0100

    bloody ie

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ jQuery(function(){
    jQuery('aside li:last-child').css("border-bottom","none", "margin-bottom", 0, "padding-bottom", 0);
    jQuery('#social li').css("border-bottom", "1px solid #e4e4e4");
    jQuery('header nav li li:last-child a').css("border-bottom:", "none", "padding-bottom", 0);
+   jQuery('#single_post p:last-child').css("margin-bottom", 0);
+   jQuery(' #comments ol ul li:last-child').css("border", "none", "padding-bottom", 0);
    
-	// MouseOver Events
+   	// MouseOver Events
 	jQuery('.thumb').hover(function(){
 		jQuery('img', this).fadeTo("fast", 0.35).addClass('box-hover');},
 	function(){
